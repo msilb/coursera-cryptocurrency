4cd332bc6e1ff70302d5b623244d196e9a0b29f5
Cristian Ghezzi <xtianus@users.noreply.github.com>
2017-12-30 13:27:02 +0100

    Recursive solution
    This is my approach to the problem. It is recursive and gets killed by the automatic evaluator so I can't say it's the correct answer.
    I tested it on a very simple use case though and it worked.

--- assignment-1-scroogecoin/MaxFeeTxHandler.java
+++ assignment-1-scroogecoin/MaxFeeTxHandler.java
@@ -1,11 +1,12 @@
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class MaxFeeTxHandler {
-
-    private UTXOPool utxoPool;
+	
+	private UTXOPool ledger;
 
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
@@ -13,87 +14,104 @@ public class MaxFeeTxHandler {
      * constructor.
      */
     public MaxFeeTxHandler(UTXOPool utxoPool) {
-        this.utxoPool = new UTXOPool(utxoPool);
+    	ledger = new UTXOPool(utxoPool);
     }
 
-    /**
-     * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
-     * (2) the signatures on each input of {@code tx} are valid,
-     * (3) no UTXO is claimed multiple times by {@code tx},
-     * (4) all of {@code tx}s output values are non-negative, and
-     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     * values; and false otherwise.
-     */
-    public boolean isValidTx(Transaction tx) {
-        UTXOPool uniqueUtxos = new UTXOPool();
-        double previousTxOutSum = 0;
-        double currentTxOutSum = 0;
-        for (int i = 0; i < tx.numInputs(); i++) {
-            Transaction.Input in = tx.getInput(i);
-            UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
-            Transaction.Output output = utxoPool.getTxOutput(utxo);
-            if (!utxoPool.contains(utxo)) return false;
-            if (!Crypto.verifySignature(output.address, tx.getRawDataToSign(i), in.signature))
-                return false;
-            if (uniqueUtxos.contains(utxo)) return false;
-            uniqueUtxos.addUTXO(utxo, output);
-            previousTxOutSum += output.value;
-        }
-        for (Transaction.Output out : tx.getOutputs()) {
-            if (out.value < 0) return false;
-            currentTxOutSum += out.value;
-        }
-        return previousTxOutSum >= currentTxOutSum;
+    private boolean isValidTx(Transaction tx, UTXOPool currentLedger) {
+    	Set<UTXO> claimedAlready = new HashSet<>();
+    	double inputSum=0, outputSum=0;
+    	// (1) all outputs claimed by {@code tx} are in the current UTXO pool
+    	// Input --> (hash, index) --> new UTXO --> contained in pool
+    	// (2) the signatures on each input of {@code tx} are valid
+    	// Input --> sig
+    	// tx --> message
+    	// Input --> (hash, index) --> new UTXO --> output --> pk
+    	int inputIndex = 0;
+    	for (Transaction.Input input : tx.getInputs()) {
+    		UTXO toFind = new UTXO(input.prevTxHash, input.outputIndex);
+    		// Check (1)
+    		if (!currentLedger.contains(toFind)) {
+    			return false; // (1) fails
+    		}
+    		// Check (2)
+    		Transaction.Output spentOutput = currentLedger.getTxOutput(toFind);
+    		inputSum += spentOutput.value;
+    		if (!Crypto.verifySignature(spentOutput.address, tx.getRawDataToSign(inputIndex++), input.signature)) {
+    			return false; // (2) fails
+    		}
+    		// (3) no UTXO is claimed multiple times by {@code tx}
+    		if (claimedAlready.contains(toFind)) {
+    			return false; // (3) fails
+    		}
+    		claimedAlready.add(toFind);
+    	}
+    	// (4) all of {@code tx}s output values are non-negative
+    	for (Transaction.Output output : tx.getOutputs()) {
+    		outputSum += output.value;
+    		if (output.value<0) {
+    			return false; // (4) fails
+    		}
+    	}
+    	// (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+    	return inputSum >= outputSum;
     }
-
-    private double calcTxFees(Transaction tx) {
-        double sumInputs = 0;
-        double sumOutputs = 0;
-        for (Transaction.Input in : tx.getInputs()) {
-            UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
-            if (!utxoPool.contains(utxo) || !isValidTx(tx)) continue;
-            Transaction.Output txOutput = utxoPool.getTxOutput(utxo);
-            sumInputs += txOutput.value;
-        }
-        for (Transaction.Output out : tx.getOutputs()) {
-            sumOutputs += out.value;
-        }
-        return sumInputs - sumOutputs;
-    }
-
+    
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
      * transaction for correctness, returning a mutually valid array of accepted transactions, and
      * updating the current UTXO pool as appropriate.
+     * Finds a set of transactions with maximum total transaction fees -- i.e. maximize the sum over 
+     * all transactions in the set of (sum of input values - sum of output values)).
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-
-        Set<Transaction> txsSortedByFees = new TreeSet<>((tx1, tx2) -> {
-            double tx1Fees = calcTxFees(tx1);
-            double tx2Fees = calcTxFees(tx2);
-            return Double.valueOf(tx2Fees).compareTo(tx1Fees);
-        });
-
-        Collections.addAll(txsSortedByFees, possibleTxs);
-
-        Set<Transaction> acceptedTxs = new HashSet<>();
-        for (Transaction tx : txsSortedByFees) {
-            if (isValidTx(tx)) {
-                acceptedTxs.add(tx);
-                for (Transaction.Input in : tx.getInputs()) {
-                    UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
-                    utxoPool.removeUTXO(utxo);
-                }
-                for (int i = 0; i < tx.numOutputs(); i++) {
-                    Transaction.Output out = tx.getOutput(i);
-                    UTXO utxo = new UTXO(tx.getHash(), i);
-                    utxoPool.addUTXO(utxo, out);
-                }
-            }
-        }
-
-        Transaction[] validTxArray = new Transaction[acceptedTxs.size()];
-        return acceptedTxs.toArray(validTxArray);
+    	List<Transaction> available = new ArrayList<Transaction>(Arrays.asList(possibleTxs));
+    	List<Transaction> chain = new ArrayList<>();
+    	UTXOPool currentLedger = new UTXOPool(ledger);
+    	handleTxsRecurse(available, chain, currentLedger);
+    	return chain.toArray(new Transaction[chain.size()]);
     }
+    
+    private double handleTxsRecurse(List<Transaction> available, List<Transaction> chain, UTXOPool currentLedger) {
+    	List<Transaction> maxChain = new ArrayList<>(chain);
+    	double maxFees=0;
+    	for (Transaction transaction : available) {
+    		if (isValidTx(transaction, currentLedger)) {
+    			UTXOPool newLedger = new UTXOPool(currentLedger);
+    			chain.add(transaction);
+    			double transactionFee = addAndCalcFee(transaction, newLedger);
+    			List<Transaction> newAvailable = new ArrayList<Transaction>(available);
+    			newAvailable.remove(transaction);
+    			List<Transaction> newChain = new ArrayList<Transaction>(chain);
+    			double chainFees = transactionFee + handleTxsRecurse(newAvailable, newChain, newLedger);
+        		if (chainFees>maxFees) {
+        			maxFees = chainFees;
+        			maxChain = newChain;
+        		}
+        		chain.remove(transaction);
+    		}
+		}
+    	chain.clear();
+    	chain.addAll(maxChain);
+    	return maxFees;
+    }
+    
+    private double addAndCalcFee(Transaction tx, UTXOPool currentLedger) {
+    	double inputSum=0, outputSum=0;
+		// Remove consumed
+		for (Transaction.Input input : tx.getInputs()) {
+			UTXO toConsume = new UTXO(input.prevTxHash, input.outputIndex);
+    		Transaction.Output spentOutput = currentLedger.getTxOutput(toConsume);
+    		inputSum += spentOutput.value;
+			currentLedger.removeUTXO(toConsume);
+		}
+		// Add unspent
+		int index=0;
+		for (Transaction.Output output : tx.getOutputs()) {
+			outputSum += output.value;
+			UTXO toAdd = new UTXO(tx.getHash(), index++);
+			currentLedger.addUTXO(toAdd, output);
+		}
+		return inputSum-outputSum;
+    }
+
 }
